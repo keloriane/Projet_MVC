7555b940b29bfebedbb966e05b4130214dbac164
Anda <ax.fiolle@gmail.com>
2017-11-06 14:33:09 +0100

    update header

--- views/header.view.php
+++ views/header.view.php
@@ -1,9 +1,14 @@
 <header>
+   <div id="description">
+       Antiquités &bull; Décoration &bull; Agencement
+   </div>
     <nav>
         <ul>
-            <li><a href="/">Accueil</a></li>
+            <li><a href="./"><img src="./assets/img/logo.png" alt="Accueil" /></a></li>
             <li><a href="./presentation">Présentation</a></li>
             <li><a href="./contact">Contact</a></li>
         </ul>
     </nav>
+    <button id="lang_switch">Français</button>
+    <div class="header_tel">+32 495/20.59.34</div>
 </header>
