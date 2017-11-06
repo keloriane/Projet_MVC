8ad8404a920eff3e2df853d54397f0e46ca83040
OrnellaSimba <ornellasimba@gmail.com>
2017-11-06 15:50:34 +0100

    footer site

--- index.php
+++ index.php
@@ -6,8 +6,9 @@
 <head>
     <meta charset="UTF-8">
     <title>Projet MVC</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 <body>
-    
+    <?php include "./views/footer.php"; ?>
 </body>
 </html>
