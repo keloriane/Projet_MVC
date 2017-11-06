d494bba5939eac6cc9c23c7a2a1c14215937e9cd
Anda <ax.fiolle@gmail.com>
2017-11-06 15:06:47 +0100

    update header + /assets/css/style.css for header

--- index.php
+++ index.php
@@ -6,8 +6,9 @@
 <head>
     <meta charset="UTF-8">
     <title>Projet MVC</title>
+    <link rel="stylesheet" href="./assets/css/style.css" />
 </head>
 <body>
-    <?php include("./views/header.view.php"); ?>
+    <?php include("./views/header.php"); ?>
 </body>
 </html>
--- views/header.php
+++ views/header.php
@@ -1,14 +1,18 @@
 <header>
-   <div class="header_description">
-       Antiquités &bull; Décoration &bull; Agencement
-   </div>
+    <div class="header_sup">
+        <div class="header_description">
+            Antiquités &bull; Décoration &bull; Agencement
+        </div>
+        <button class="lang_switch">Français</button>
+    </div>
     <nav>
+        <h1><a href="./"><img src="./assets/img/logo.png" alt="Accueil" /></a></h1>
         <ul>
-            <li><a href="./"><img src="./assets/img/logo.png" alt="Accueil" /></a></li>
+            
             <li><a href="./presentation">Présentation</a></li>
+            <li><a href="">Le stock</a></li>
             <li><a href="./contact">Contact</a></li>
         </ul>
+        <div class="header_tel">+32 495/20.59.34</div>
     </nav>
-    <button id="lang_switch">Français</button>
-    <div class="header_tel">+32 495/20.59.34</div>
 </header>
