e7cd35f650b42bd97f87b50da0bd51b2316da0b8
ksomao <somao@outlook.com>
2017-11-07 14:17:00 +0100

    routeer added

--- controller/router.php
+++ controller/router.php
@@ -1,3 +1,16 @@
 <?php
 
+$uri = explode("/", $_SERVER["REQUEST_URI"]);
+print_r($uri);
+$page = $uri[2] != "" ? $uri[2] : "presentation";
+$view = "./views/" . $page . ".php";
+$content = "";
+
+if (file_exists($view)) {
+    $content = $view;
+} else {
+    $content = "./views/error404.php";
+}
+
+include "./views/master.php";
 ?>
