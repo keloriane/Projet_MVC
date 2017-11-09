bac558b7cee687c7e29335503a5c979c6c25e626
Anda <ax.fiolle@gmail.com>
2017-11-09 16:15:00 +0100

    more rev

--- views/error404.php
+++ views/error404.php
@@ -1,3 +1,12 @@
-<div class="content">
-    <h1>ERROR 404</h1>
-</div>
+<section id="myprez" class="content-section text-center">
+  
+   <div class="row">
+    <div class="col-lg-8 mx-auto">
+        <div class="description ml-auto mx-auto">
+           <div class="content">
+                <h1>ERROR 404</h1>
+            </div>
+        </div>
+   </div>
+    </div>
+</section>
