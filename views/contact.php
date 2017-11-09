88b0e66134434ab71350e1128f754945e6a482e8
Anda <ax.fiolle@gmail.com>
2017-11-09 16:28:14 +0100

    c

--- views/contact.php
+++ views/contact.php
@@ -30,14 +30,14 @@
 
 <div id="contact"> 
     <p>Le magasin est ouvert le samedi matin de 10h00 à 12h30 ou sur rendez-vous.</p>
-    <button>Didier Motte</button>
-    <button>dmotte@skynet.be</button>
+    <button class="mybtn">Didier Motte</button>
+    <button class="mybtn">dmotte@skynet.be</button>
     <p>+32 495 20.59.34</p>
     <p> Rue Saint Georges , 35 ,</p>
     <p>1400 Nivelles ,</p>
     <p>Belgique.</p>
    <p>Retrouvez nos mises à jour en vous
-    <button>abonnant à notre page Facebook</button></p>
+    <button class="mybtn">abonnant à notre page Facebook</button></p>
 </div>
  <div id="formulaire">   
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2532.5888408897763!2d4.324954415203701!3d50.5975949846332!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c232d3694f6c59%3A0xe7333aff0bd54fec!2sRue+Saint-Georges+35%2C+1400+Nivelles!5e0!3m2!1sen!2sbe!4v1509974320504" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
