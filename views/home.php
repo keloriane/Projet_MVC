fc35ff1fc64f24674de8e36aa8c18537c690e9bc
ksomao <somao@outlook.com>
2017-11-10 06:41:45 +0100

    update folder and file

--- views/home.php
+++ views/home.php
@@ -1,32 +1,5 @@
 
 
-<!-- Navigation -->
-<!--<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-    <div class="container">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
-                aria-label="Toggle navigation">
-            Menu
-            <i class="fa fa-bars"></i>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#about">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#download">Download</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>-->
-
-<!-- Intro Header -->
 <header class="masthead">
     <div class="intro-body">
         <div class="container">
@@ -57,7 +30,6 @@
     </div>
 </section>
 
-<!-- Download Section -->
 <section id="download" class="download-section content-section text-center">
     <div class="container">
         <div class="col-lg-8 mx-auto">
@@ -67,51 +39,5 @@
         </div>
     </div>
 </section>
-
-<!-- Contact Section -->
-<!--<section id="contact" class="content-section text-center">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-8 mx-auto">
-                <h2>Contact Start Bootstrap</h2>
-                <p>Feel free to leave us a comment on the
-                    <a href="http://startbootstrap.com/template-overviews/grayscale/">Grayscale template overview
-                        page</a>
-                    on Start Bootstrap to give some feedback about this theme!</p>
-                <ul class="list-inline banner-social-buttons">
-                    <li class="list-inline-item">
-                        <a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg">
-                            <i class="fa fa-twitter fa-fw"></i>
-                            <span class="network-name">Twitter</span>
-                        </a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="https://github.com/BlackrockDigital/startbootstrap" class="btn btn-default btn-lg">
-                            <i class="fa fa-github fa-fw"></i>
-                            <span class="network-name">Github</span>
-                        </a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="https://plus.google.com/+Startbootstrap/posts" class="btn btn-default btn-lg">
-                            <i class="fa fa-google-plus fa-fw"></i>
-                            <span class="network-name">Google+</span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</section>-->
-
-<!-- Map Section -->
-<!--<div id="map"></div>-->
-
-<!-- Footer -->
-<!--<footer>
-    <div class="container text-center">
-        <p>Copyright &copy; Your Website 2017</p>
-    </div>
-</footer>-->
-
 
 
--- views/master.php
+++ views/master.php
@@ -11,15 +11,15 @@
     <title>Antiquités, Décorations, Belgique &mdash; Didier Motte &#8211; Antiquités, Décoration</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="./views/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom fonts for this template -->
-    <link href="./views/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="./assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet"
           type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Cabin:700' rel='stylesheet' type='text/css'>
     <!-- Custom styles for this template -->
-    <link href="./views/css/grayscale.min.css" rel="stylesheet">
+    <link href="./assets/css/grayscale.min.css" rel="stylesheet">
     <!--<link rel="stylesheet" href="./assets/css/style.css" />-->
 </head>
 
@@ -32,17 +32,17 @@ include "./views/footer.php";
 ?>
 
 <!-- Bootstrap core JavaScript -->
-<script src="./views/vendor/jquery/jquery.min.js"></script>
-<script src="./views/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="./assets/vendor/jquery/jquery.min.js"></script>
+<script src="./assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 <!-- Plugin JavaScript -->
-<script src="./views/vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="./assets/vendor/jquery-easing/jquery.easing.min.js"></script>
 
 <!-- Google Maps API Key - Use your own API key to enable the map feature. More information on the Google Maps API can be found at https://developers.google.com/maps/ -->
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
 
 <!-- Custom scripts for this template -->
-<script src="./views/js/grayscale.min.js"></script>
+<script src="./assets/js/grayscale.min.js"></script>
 </body>
 
 </html>
